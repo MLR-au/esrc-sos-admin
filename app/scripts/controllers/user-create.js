fe72fcd0cf818bbf0660cf7cf7f68b28113eca47
Marco La Rosa <m@lr.id.au>
2014-09-26 17:24:27 +1000

    recover create manage functionality, other stuff

--- app/scripts/controllers/user-create.js
+++ app/scripts/controllers/user-create.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('adminApp')
-  .controller('UserCreateCtrl', [ '$scope', 'AuthService', function ($scope, AuthService) {
+  .controller('UserCreateCtrl', [ '$scope', '$http', 'AuthService', 'configuration',
+        function ($scope, $http, AuthService, configuration) {
       // listen for the logged in message from the auth service
       $scope.ready = false;
       $scope.$on('user-logged-in', function() {
@@ -17,4 +18,55 @@ angular.module('adminApp')
 
       // check that the user session is ok
       AuthService.verify();
+
+          var service = configuration[configuration['service']];
+          console.log(service);
+
+          // init the userdata object
+          $scope.userdata = {};
+
+          var validateEmail = function(email) {
+              if (email !== undefined) {
+                  var url = service + '/admin/email/' + email;
+                  console.log(url);
+                  $http.get(url).then(function(resp) {
+                      console.log(resp);
+                      $scope.existingUser = resp.data.userdata;
+                  },
+                  function(resp) {
+                  });
+              }
+          }
+
+          $scope.validatePrimaryEmail = function() {
+              var result = validateEmail($scope.userdata.primaryEmail);
+          }
+
+          $scope.validateSecondaryEmail = function() {
+              var result = validateEmail($scope.userdata.secondaryEmail);
+          }
+          $scope.save = function() {
+              // ensure all is in order
+              $scope.validatePrimaryEmail();
+              $scope.validateSecondaryEmail();
+
+              if (!$scope.existingUser) {
+                  if ($scope.userdata.username !== undefined && $scope.userdata.primaryEmail !== undefined) {
+                      var url = service + '/admin/user/create';
+                      $http.post(url, $scope.userdata).then(function(resp) {
+                        $scope.userCreated = true;
+                      },
+                      function(resp) {
+                          $scope.failure = true;
+                      });
+                  }
+              }
+          }
+
+          $scope.reset = function() {
+              $scope.existingUser = undefined;
+              $scope.userCreated = false;
+              $scope.failure = false;
+              $scope.userdata = {};
+          }
   }]);
--- app/scripts/controllers/user-manage.js
+++ app/scripts/controllers/user-manage.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('adminApp')
-  .controller('UserManageCtrl', [ '$scope', 'AuthService', function ($scope, AuthService) {
+  .controller('UserManageCtrl', [ '$scope', '$http', 'AuthService', 'configuration', 
+        function ($scope, $http, AuthService, configuration) {
       // listen for the logged in message from the auth service
       $scope.ready = false;
       $scope.$on('user-logged-in', function() {
@@ -17,4 +18,68 @@ angular.module('adminApp')
 
       // check that the user session is ok
       AuthService.verify();
+
+          var service = configuration[configuration['service']];
+          console.log(service);
+
+          $scope.loadUsers = function() {
+              var url = service + '/admin/users';
+               $http.get(url).then(function(resp) {
+                   $scope.users = resp.data.users;
+                   console.log($scope.users);
+               },
+               function(resp) {
+               });
+
+          }
+          $scope.loadUsers();
+
+          $scope.allowAccess = function(userid, app) {
+              console.log('allow', userid, app);
+              var url = service + '/admin/user/' + userid;
+              var data = {
+                  'app': app,
+                  'action': 'allowAccess'
+              }
+              $http.put(url, data).then(function(resp) {
+              },
+              function(resp) {
+              })
+          }
+
+          $scope.removeAccess = function(userid, app) {
+              console.log('remove', userid, app);
+              var url = service + '/admin/user/' + userid;
+              var data = {
+                  'app': app,
+                  'action': 'denyAccess'
+              }
+              $http.put(url, data).then(function(resp) {
+              },
+              function(resp) {
+              })
+          }
+
+          $scope.lockAccount = function(userid) {
+              console.log('lock', userid);
+              var url = service + '/admin/user/' + userid;
+              var data = {
+                  'action': 'lockAccount'
+              }
+              $http.put(url, data).then(function(resp) {
+                  console.log(resp);
+              },
+              function(resp) {
+              })
+          }
+
+          $scope.deleteAccount = function(userid) {
+              console.log('delete', userid);
+              var url = service + '/admin/user/' + userid;
+              $http.delete(url).then(function(resp) {
+                  $scope.loadUsers();
+              },
+              function(resp) {
+              })
+          }
   }]);
