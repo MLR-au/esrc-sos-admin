08b27e20d8f48b7734d11cc95f2f0e6fb850103b
Marco La Rosa <m@lr.id.au>
2014-10-13 13:08:17 +1100

    Built admin from commit 9ab6392 on branch master

--- app/scripts/controllers/login.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('adminApp')
-  .controller('LoginCtrl', [ '$scope', 'AuthService', function ($scope, AuthService) {
-      AuthService.getToken();
-  }]);
--- app/scripts/services/configuration.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('adminApp')
-  .constant('configuration', {
-      'development': 'https://essos.esrc.info',
-      'testing': '',
-      'production': 'https://sos.esrc.unimelb.edu.au',
-      'service': 'production'
-  });
